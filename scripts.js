1a3d4c001d10a70a45ad1ed01839ba8083798185
abdihakim <55029103+abdikeem@users.noreply.github.com>
2019-09-30 09:58:06 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+     $(".main").click(function(){
+     $("#design").toggle();
+     $("#space").toggle();
+  });
+     $(".mai").click(function(){
+     $("#development").toggle();
+    $("#road").toggle();
+  });
+    $(".kim").click(function(){
+    $("#product").toggle();
+    $("#craft").toggle();
+  });
+
+});
+
+$(document).ready(function(){
+  $("form#form").submit(function(event){
+    var name= $("input#userInfo").val();
+    var email= $("input#gmail").val();
+    var massage= $("input#sms").val();
+    if($("input#userInfo").val() && $("input#gmail").val()){
+      alart(name + ", Your massage has been received.Thank you for reaching out to us.");
+    }
+    else{
+      alert("incorrect your name and email!");
+    }
+  });
+});
